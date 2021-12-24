7ef47a9fcdd2e05927d7e806e6760537224684a3
Pascal Knoll <knollpascal00@gmail.com>
2021-12-24 12:27:18 +0100

    ^12

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from enviroments.generate_envs import generate_random_env
 from utils import create_nx_graph
-
+from objects import EnvBlueprint
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    routes, _, _ = generate_random_env(max_n_stations=30)
-    create_nx_graph(routes[0], routes[1])
-    plt.show()
+    env = EnvBlueprint()
+    env.read_txt("input/input2.txt")
+    env.render()
